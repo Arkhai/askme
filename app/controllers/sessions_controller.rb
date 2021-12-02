b6081186a100e9875689e3f81c962ead3a082020
arkhai <kiriam@bk.ru>
2021-12-02 22:15:54 +0300

    Controller session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Вы успешно залогинились!'
+    else
+      flash.now.alert = 'Неправильно введен логин и/или пароль!'
+      render :new
+    end
   end
 
   def destroy
