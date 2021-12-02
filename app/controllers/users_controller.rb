3858da358efcfe092bf27d5116cf803c993def8f
arkhai <kiriam@bk.ru>
2021-12-02 21:49:53 +0300

    Controller for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :create, :new]
+
   def index
     @users = User.all
   end
@@ -18,17 +21,28 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find params[:id]
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Ваш профиль успешно обновлён!'
+    else
+      render 'edit'
+    end
   end
 
   def show
-    @user = User.find params[:id]
     @questions = @user.questions.order(created_at: :desc)
 
     @new_question = @user.questions.build
   end
 
   private
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
