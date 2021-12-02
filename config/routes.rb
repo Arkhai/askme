dc88d2865d61923cac99ded85eb3754ed811a500
arkhai <kiriam@bk.ru>
2021-12-02 22:09:33 +0300

    View for new session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   root 'users#index'
 
   resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
