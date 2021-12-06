b1d247a90b91e78e95594c353a57d73174e830df
arkhai <kiriam@bk.ru>
2021-12-06 16:37:49 +0300

    auto login after creating new profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь зарегистрирован!'
     else
       render 'new'
@@ -36,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user.questions.destroy
     @user.destroy
     redirect_to root_path, notice: 'Ваш аккаунт успешно удалён.'
   end
