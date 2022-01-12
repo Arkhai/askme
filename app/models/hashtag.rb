e30d5912c39fcb8b69809cfae3129fc813bd4cf2
arkhai <kiriam@bk.ru>
2022-01-12 22:30:46 +0300

    Hashtags views

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -5,6 +5,4 @@ class Hashtag < ApplicationRecord
   has_many :questions, through: :hashtag_questions
   
   scope :with_questions, -> { where_exists(:hashtag_questions) }
-  
-  validates :text, uniqueness: true, presence: true
 end
